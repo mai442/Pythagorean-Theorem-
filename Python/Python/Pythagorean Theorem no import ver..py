67c2fb6bff40c2473aa3db10d4f387f7580351e2
paitoonx <poggpi979@gmail.com>
2026-01-31 13:22:33 +0700

    Add files via upload
    add no import ver.

---
+++ Python/Python/Pythagorean Theorem no import ver..py
@@ -0,0 +1,6 @@
+print("Welcome to the Pythagorean Theorem Calculator!")
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+
+c = (a**2 + b**2) ** 0.5
+print(f"The length of the hypotenuse c is: {c}")
---
+++ Python/Python/Pythagorean Theorem.py
@@ -0,0 +1,12 @@
+import math
+
+def pythagorean_theorem(a, b):
+    return math.hypot(a, b)
+
+print("Pythagorean Theorem Calculator")
+
+a = float(input("Enter length of side a: "))
+b = float(input("Enter length of side b: "))
+c = pythagorean_theorem(a, b)
+
+print(f"The length of the hypotenuse c is: {c}")
